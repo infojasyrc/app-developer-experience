a33834318f423c497c0f3dd9737dd7012b9f650a
Jose Sal y Rosas <infojasyrc@gmail.com>
2024-11-27 00:17:02 -0500

    chore: upgrade all dependencies

--- src/common/utils/http-client-utils.ts
+++ src/common/utils/http-client-utils.ts
@@ -1,12 +1,9 @@
 import { ConfigService } from '@nestjs/config';
-import { EnvironmentVariables } from 'src/infrastructure/environment-variables';
+import { EnvironmentVariables } from './../../infrastructure/environment-variables';
 import { AxiosRequestHeaders } from 'axios';
 
 export const getAPIRequestHeaders = (
   config: ConfigService<EnvironmentVariables>,
-): AxiosRequestHeaders => ({
-  'X-Api-Version': '3',
-  Authorization: `Bearer ${config.get('INTEGRATION_API_TOKEN')}`,
-  Accept: 'application/json',
-  'Content-Type': 'application/json',
-});
+): AxiosRequestHeaders => (
+  config.get('INTEGRATION_API_TOKEN')
+);
