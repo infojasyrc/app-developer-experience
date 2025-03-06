2c9158f0d62e8b5c60e42b2cc19f1793aba29ba2
Jose Sal y Rosas <infojasyrc@gmail.com>
2025-03-06 13:00:55 -0500

    feat: add events model and admin (#39)
    * feat: added events model and enable admin for events
    
    * feat: added events model and migration for admin

--- conference-manager/ms-conference-admin/src/manager/models.py
+++ conference-manager/ms-conference-admin/src/manager/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 
 
 class Headquarter(models.Model):
+    """ Headquarter model """
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
@@ -19,6 +20,29 @@ class Headquarter(models.Model):
     class Meta:
         verbose_name_plural = 'Headquarters'
         db_table = 'headquarters'
+        managed = False
+
+    def __str__(self):
+        return self.name
+
+
+class Conference(models.Model):
+    """ Conference model """
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    event_type = models.CharField(max_length=100)
+    headquarter = models.ForeignKey(Headquarter, on_delete=models.CASCADE)
+    status = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    #created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    #updated_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'Conferences'
+        db_table = 'events'
         managed = False
 
     def __str__(self):
