a8e8de87516cf60f7b40ef6e30049cc98186933d
Jose Sal y Rosas <infojasyrc@gmail.com>
2025-03-19 16:49:11 -0500

    feat: solved all issues to run the application using react query

--- conference-manager/ms-conference-webapp/src/app/layout.tsx
+++ conference-manager/ms-conference-webapp/src/app/layout.tsx
@@ -1,10 +1,8 @@
-"use client";
-
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Providers } from "./providers";
 
-import "globals.css";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- conference-manager/ms-conference-webapp/src/app/providers.tsx
+++ conference-manager/ms-conference-webapp/src/app/providers.tsx
@@ -4,8 +4,8 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-import { queryClient } from "../lib/queryClient";
-import { isDevelopment } from "../shared/environment";
+import { queryClient } from "./lib/queryClient";
+import { isDevelopment } from "./shared/environment";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
