bba95d8a956305fd16a19d0bb6f1936611eb5fd8
Jose Sal y Rosas <infojasyrc@gmail.com>
2025-03-21 10:46:54 -0500

    refactor: moved all hooks and lib inside shared folder (#52)
    * refactor: moved all hooks and lib inside shared folder
    
    * feat: modified target branch and specify branches to run specific tasks

--- conference-manager/ms-conference-webapp/src/app/providers.tsx
+++ conference-manager/ms-conference-webapp/src/app/providers.tsx
@@ -4,7 +4,7 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-import { queryClient } from "./lib/queryClient";
+import { queryClient } from "./shared/lib/queryClient";
 import { isDevelopment } from "./shared/environment";
 
 export function Providers({ children }: { children: React.ReactNode }) {
