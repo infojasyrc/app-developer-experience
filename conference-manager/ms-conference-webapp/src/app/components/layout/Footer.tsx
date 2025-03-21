9cbc417996bc64669294caec7307466255aaafcc
Jose Sal y Rosas <infojasyrc@gmail.com>
2025-03-21 13:05:56 -0500

    format: format footer component

--- conference-manager/ms-conference-webapp/src/app/components/layout/Footer.tsx
+++ conference-manager/ms-conference-webapp/src/app/components/layout/Footer.tsx
@@ -5,46 +5,46 @@ export default function Footer() {
   return (
     <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center fixed bottom-0 left-0 right-0 bg-white p-4">
       <a
-      className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-      href=""
-      target="_blank"
-      rel="noopener noreferrer"
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href=""
+        target="_blank"
+        rel="noopener noreferrer"
       >
-      <Image
-        aria-hidden
-        src="/file.svg"
-        alt="File icon"
-        width={16}
-        height={16}
-      />
+        <Image
+          aria-hidden
+          src="/file.svg"
+          alt="File icon"
+          width={16}
+          height={16}
+        />
       </a>
       <a
-      className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-      href=""
-      target="_blank"
-      rel="noopener noreferrer"
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href=""
+        target="_blank"
+        rel="noopener noreferrer"
       >
-      <Image
-        aria-hidden
-        src="/window.svg"
-        alt="Window icon"
-        width={16}
-        height={16}
-      />
+        <Image
+          aria-hidden
+          src="/window.svg"
+          alt="Window icon"
+          width={16}
+          height={16}
+        />
       </a>
       <a
-      className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-      href=""
-      target="_blank"
-      rel="noopener noreferrer"
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href=""
+        target="_blank"
+        rel="noopener noreferrer"
       >
-      <Image
-        aria-hidden
-        src="/globe.svg"
-        alt="Globe icon"
-        width={16}
-        height={16}
-      />
+        <Image
+          aria-hidden
+          src="/globe.svg"
+          alt="Globe icon"
+          width={16}
+          height={16}
+        />
       </a>
     </footer>
   );
