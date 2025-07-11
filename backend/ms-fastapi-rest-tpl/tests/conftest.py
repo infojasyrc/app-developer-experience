1c868435aad7d24928d9ea0ba1ae2e61b21ed2ce
Jose Sal y Rosas <infojasyrc@gmail.com>
2025-07-10 23:41:10 -0500

    feat(fastapi-rest-tpl): added unit tests but not running

--- backend/ms-fastapi-rest-tpl/tests/conftest.py
+++ backend/ms-fastapi-rest-tpl/tests/conftest.py
@@ -1,10 +1,18 @@
 import pytest
+import os
+import sys
+
+src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src'))
+sys.path.insert(0, src_path)
+
+
 from fastapi.testclient import TestClient
 
 from src.application import get_application
 
 
-def get_test_client() -> TestClient:
+@pytest.fixture
+def test_client() -> TestClient:
     """
     Create a test client for the FastAPI application.
     This client can be used to make requests to the application during tests.
