793c1483d9e2ce37934de8dd445f354553ff8377
Jose Sal y Rosas <infojasyrc@gmail.com>
2025-11-04 08:12:10 -0500

    chore(cloud-aws): upgrade dependencies and added makefile to handle all commands

--- cloud/terraform/aws/versions.tf
+++ cloud/terraform/aws/versions.tf
@@ -3,15 +3,15 @@ terraform {
     #Deploying AWS
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.4"
+      version = "~> 6.0"
     }
 
   }
-  required_version = ">= 0.15.4"
+  required_version = ">= 1.13.0"
 }
 
 # Configure the AWS Provider and Profiles in aws credentials
 provider "aws" {
-  region  = "us-east-1"
-  profile = "demo-${terraform.workspace}"
+  region  = "us-west-1"
+  profile = "terraform-dev"
 }
