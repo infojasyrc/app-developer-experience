4fd06b39427ccdc9f957a54a3dccdd74408e5bcb
Jose Sal y Rosas <infojasyrc@gmail.com>
2026-02-07 16:17:14 -0500

    feat(cloud-aws): added backend in versions and use it in pull request pipeline

--- cloud/terraform/aws/versions.tf
+++ cloud/terraform/aws/versions.tf
@@ -1,13 +1,14 @@
 terraform {
+  required_version = ">= 1.13.0"
+  backend "s3" {
+  }
   required_providers {
     #Deploying AWS
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
-
   }
-  required_version = ">= 1.13.0"
 }
 
 # Configure the AWS Provider and Profiles in aws credentials
