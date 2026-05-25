f631b153d546a472d603fe267240dba228121137
Jose Sal y Rosas <infojasyrc@gmail.com>
2026-05-25 17:57:47 -0500

    feat(cm-api): removed additionally unused code

--- conference-manager/ms-conference-api/src/modules/users/dto/user-uid.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger'
-import { IsNotEmpty, IsString } from 'class-validator'
-
-export class UserUidDto {
-  @ApiProperty({ description: 'Firebase auth uid' })
-  @IsString()
-  @IsNotEmpty()
-  uid!: string
-}
